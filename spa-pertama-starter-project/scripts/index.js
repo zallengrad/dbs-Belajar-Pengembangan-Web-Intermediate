49a755272c485a2c16866a5c91c379a316d82c2b
Muhammad Afrizale <141467689+zallengrad@users.noreply.github.com>
2025-04-25 14:47:37 +0700

    SPA - projek awal

---
+++ spa-pertama-starter-project/scripts/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  console.log('Hello, world!');
+});
+
+window.addEventListener('hashchange', async () => {
+  console.log('Hash changed!');
+});
